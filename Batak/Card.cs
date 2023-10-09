0dc669f4cdea8d1bb856293ae3fb2f495a494344
iberatd <beratduzenli.01@gmail.com>
2023-10-09 20:57:19 +0300

    Card

---
+++ Batak/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batak
+{
+    class Card
+    {
+        private string color;
+        private string name;
+        private int value;
+
+        private static List<String> colors = new List<string> { "Sinek","Karo","Maça","Kupa"};
+        private static List<String> royalty = new List<string> { "Vale", "Kız", "Papaz", "As" };
+
+        public int Value { get => value; set => this.value = value; }
+        public string Name { get => name; set => name = value; }
+        public string Color { get => color; set => color = value; }
+        public static List<string> Colors { get => colors; set => colors = value; }
+        public static List<string> Royalty { get => royalty; set => royalty = value; }
+
+        public Card() { }
+
+        public Card(int number)
+        {
+            Color = Colors[number / 13];
+
+            Value = number % 13;
+            if (Value > 8)
+                Name = Royalty[Value - 9];
+            else
+                Name = (Value + 2).ToString();
+        }
+
+
+    }
+}
