ef455cb932f497e88c4aed18dc05528365e29727
iberatd <beratduzenli.01@gmail.com>
2023-10-10 00:07:27 +0300

    Card Ids 1 - 52

--- Batak/Card.cs
+++ Batak/Card.cs
@@ -11,20 +11,23 @@ namespace Batak
         private string color;
         private string name;
         private int value;
+        private int id;
 
-        private static List<String> colors = new List<string> { "Sinek","Karo","Maça","Kupa"};
-        private static List<String> royalty = new List<string> { "Vale", "Kız", "Papaz", "As" };
+        private static List<String> colors = new List<string> { "♣", "♦", "♠", "♥" };
+        private static List<String> royalty = new List<string> { "J", "Q", "K", "A" };
 
         public int Value { get => value; set => this.value = value; }
         public string Name { get => name; set => name = value; }
         public string Color { get => color; set => color = value; }
         public static List<string> Colors { get => colors; set => colors = value; }
         public static List<string> Royalty { get => royalty; set => royalty = value; }
+        public int Id { get => id; set => id = value; }
 
         public Card() { }
 
         public Card(int number)
         {
+            Id = number+1;
             Color = Colors[number / 13];
 
             Value = number % 13;
@@ -33,7 +36,5 @@ namespace Batak
             else
                 Name = (Value + 2).ToString();
         }
-
-
     }
 }
